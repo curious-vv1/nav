f30911357e15758053ee917b3f541cc1de010ce5
curious-vv1 <vivekrealc2@gmail.com>
2024-03-19 22:31:40 +0530

    feat:project

---
+++ turtle_ws/build/turtlebot3_fake_node/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vivek/nav/turtle_ws/src/turtlebot3_fake_node
+# Build directory: /home/vivek/nav/turtle_ws/build/turtlebot3_fake_node
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ turtle_ws/build/turtlebot3_simulations/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/vivek/nav/turtle_ws/install/turtlebot3_fake_node/share/turtlebot3_fake_node/package.sh"
+. "/home/vivek/nav/turtle_ws/install/turtlebot3_gazebo/share/turtlebot3_gazebo/package.sh"
